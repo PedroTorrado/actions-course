aedd360970108b628ebc1d2c4a5c702c3e9277d0
PedroTorrado <88887442+PedroTorrado@users.noreply.github.com>
2024-08-05 17:05:44 +0100

    major update

---
+++ .changeset/cyan-plants-shout.md
@@ -0,0 +1,5 @@
+---
+"next-1": major
+---
+
+Updated e2e testing, and page
--- e2e/home-link.spec.ts
+++ e2e/home-link.spec.ts
@@ -1,12 +1,44 @@
 import { test, expect } from "@playwright/test";
 
-test("should navigate to the about page", async ({ page }) => {
+test("should display the correct header elements", async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto("http://localhost:3000/");
-  // Find an element with the text 'About Page' and click on it
+
+  // Verify the presence of the header elements
+  await expect(page.locator("h1")).toHaveText("Welcome to Tech Insights");
+  await expect(page.locator("h2")).toHaveText("Hello World");
+});
+
+test("should display the navigation links", async ({ page }) => {
+  // Start from the index page
+  await page.goto("http://localhost:3000/");
+
+  // Verify the presence of the navigation links
+  const navLinks = ["About", "Contact", "Blog"];
+  for (const linkText of navLinks) {
+    await expect(page.locator(`text=${linkText}`)).toBeVisible();
+  }
+});
+
+test("should display the search input", async ({ page }) => {
+  // Start from the index page
+  await page.goto("http://localhost:3000/");
+
+  // Verify the presence of the search input
+  const searchInput = page.locator('input[placeholder="Search"]');
+  await expect(searchInput).toBeVisible();
+});
+
+test("should navigate to the about page", async ({ page }) => {
+  // Start from the index page
+  await page.goto("http://localhost:3000/");
+
+  // Navigate to the About page
   await page.click("text=About");
+
   // The new URL should be "/about" (baseURL is used there)
   await expect(page).toHaveURL("http://localhost:3000/about");
-  // The new page should contain an h1 with "About Page"
+
+  // The new page should contain an h2 with "About"
   await expect(page.locator("h2")).toContainText("About");
 });
